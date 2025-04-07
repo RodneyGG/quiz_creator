69a9cb8a15ccea33a932a7b8fa7ddf8025edcfc6
RodneyGG <rodneyarevalo9963@gmail.com>
2025-04-07 19:21:09 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,15 @@
+"""
+For Assignment 9: Quiz Creator
+Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and 
+the correct answer. Write the collected data to a text file. Ask another question until the user chose 
+to exit.
+"""
+
+#Ask the user what subject or topic the question will he or she be making
+#Make a filename named "{Topic or subject}_questions.txt" to store the question
+#Ask the user to input a question
+#input choices for A, B, C, D
+#select what is the correct answer
+#All of the input of the user will be stored in a text file created
+#ask the user again to input a question or quit the program
+
