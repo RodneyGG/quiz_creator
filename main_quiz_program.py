e27338ffd9c6f9b1abd6d1ddd3082696a7494eb2
RodneyGG <rodneyarevalo9963@gmail.com>
2025-05-02 00:06:37 +0800

    added the logic for the code and there is a welcome text when you start the program

--- main_quiz_program.py
+++ main_quiz_program.py
@@ -1,4 +1,24 @@
 #Make a Program that calls quiz taker and quiz creator in just one file
 #Import quiz creator and quiz taker
+import quiz_taker
+import quiz_creator
+import pyfiglet
+
+def welcome_text():
+    ascii_banner = pyfiglet.figlet_format("Quiz Forge", font="doom")
+    print(ascii_banner)
+
 #make a choice if it want to make or take a quiz
-#quit the program
+main_quiz_program = True
+welcome_text()
+while main_quiz_program:
+    choice = input("Please Type The Number\n1 = Quiz Creator\n2 = Quiz Taker\n3 = Exit\n").strip()
+    if choice == "1":
+        quiz_creator.main()
+    elif choice == "2":
+        quiz_taker.main()
+    elif choice == "3":#quit the program
+        main_quiz_program = False
+    else:
+        print("PLEASE ONLY ENTER 1,2,3. Thank YOU")
+
