350e4bde19cd238a8f043a551876fb0fb0b23827
RodneyGG <rodneyarevalo9963@gmail.com>
2025-05-01 13:39:29 +0800

    psudocode for quiz_taker

---
+++ quiz_taker.py
@@ -0,0 +1,13 @@
+"""
+For Assignment 10: Quiz
+Create the Quiz program that read the output file of the Quiz Creator. 
+The user will answer the randomly selected question and check if the answer is correct.
+"""
+#ask the user for the topic of the exam
+#Ask the user for its full name 
+#the program will load the exam
+#the program will randomize the order of the question
+#the program will randomize the choices 
+#It will then start a quiz and set the score to 0. Every correct answer is plus 1 point
+#print the score of the quiz taker
+#ask for the quiz taker email and it will  email the score and the quiz to her 
